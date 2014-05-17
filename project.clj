ff8da155bdc126f31faa4a89515e59212efe3ed5
Laurent Petit <laurent.petit@gmail.com>
2014-05-17 19:24:03 +0200

    fix(mvn): ccw.server name instead of ccw-server

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ccw/ccw-server "0.1.0"
+(defproject ccw/ccw.server "0.1.0"
   :description "Library that must be placed in your project classpath when you connect to it via CCW over nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
